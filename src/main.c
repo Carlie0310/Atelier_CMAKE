707be9c47357c44fde4cafbe3f32e7287a37facb
Carole FOBASSO <77639832+Carlie0310@users.noreply.github.com>
2025-06-04 10:34:04 +0200

    Update main.c
    Updated main.c

--- src/main.c
+++ src/main.c
@@ -4,6 +4,23 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
+	if (argc == 3) {
+        char *op = argv[1];
+        char *x_str = argv[2];
+        double résultat = 0;
+
+		if (strcmp(op, "car") == 0) {
+	// Convertir x_str en double, appeler _car et afficher le résultat
+	        double x = atof(x_str);
+	        résultat = _car(x);
+		printf("%lf", résultat);
+	        return 0;
+	        }
+        // Si ce n'est pas "car", on tombe sur l'erreur de paramètres
+	        printf("Erreur de parametres\n");
+	        return 1;
+    	}
+	
 	if (argc == 4)
 	{
 	char* op = argv[1];
